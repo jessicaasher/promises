a0193226d355659cf09460297b1f36a97957aba8
Jessie A <jasherworks@gmail.com>
2026-07-10 13:45:06 -0700

    written explanations on each line for clarity

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 // the backend has given us a promise. to unlock what's inside, we want to use then or async/await.
 
 //1. Then
-(fetch("https://jsonplaceholder.typicode.com/users/1")).then(response => {
-    console.log(response.json())
-    response.json().then(data => {
-        console.log(data)
+(fetch("https://jsonplaceholder.typicode.com/users/1")).then(response => { // response is a promise, so we need to use then to get the data 
+    response.json().then(data => { // response.json() is also a promise, so we need to use then again to get the data
+        console.log(data) // data is the actual data we want to use
     })
 })
