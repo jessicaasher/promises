21201767cfa9d4a164e9dbae1d54cd9e724c0abe
Jessie A <jasherworks@gmail.com>
2026-07-10 11:01:43 -0700

    because its just one argument, we don't need those parentheses

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // the backend has given us a promise. to unlock what's inside, we want to use then or async/await.
 
 //1. Then
-(fetch("https://jsonplaceholder.typicode.com/users/1")).then((response) => {
+(fetch("https://jsonplaceholder.typicode.com/users/1")).then(response => {
 
 
 })
