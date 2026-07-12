c0a815ba7c5c27fddb1690065613e235807b3cfd
Jessie A <jasherworks@gmail.com>
2026-07-12 10:09:41 -0700

    bc its just one argument, we don't need parentheses

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
 
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
 
 })
