de550cb9249d0e685db94885b9635b6df3899583
Jessie A <jasherworks@gmail.com>
2026-07-12 10:08:50 -0700

    inside is where we get the response from the backend server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
 
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(() => {
-    
+fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+
 })
