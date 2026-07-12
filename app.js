4e9ad0e97e7ef92a856e5ebd048b78b1ec5f8ad0
Jessie A <jasherworks@gmail.com>
2026-07-12 10:34:11 -0700

    lets console log this to make sure its correct

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
-const emailRef = document.querySelector(".email")
+const emailRef = document.querySelector(".email");
+console.log(emailRef)
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
   response.json().then(data => {
