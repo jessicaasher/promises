8758399a3d2797293c5f84bf4b14f3854dfbfd29
Jessie A <jasherworks@gmail.com>
2026-07-13 15:37:03 -0700

    First, we fetch the backend. This is a promise in and of itself.  We unlock this promise by using a .then.  We get a response. We accept it through the argument. The response is pure gibberish to the frontend so we need to make it something the frontend will understand with a .json(). response.json() takes time, it is a promise in and of itself, so we return it. When we return it, the whole fetch().then() becomes a promise that we're returning.  To unlock this promise, we use a .then again. Inside the .then() is a callback. It has the data and we console.log(data) and we get the response we were getting before when we nested the .then's inside of each other

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
 const emailRef = document.querySelector(".email");
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+fetch("https://jsonplaceholder.typicode.com/users/1")
+.then(response => {
   return response.json()
-}) 
-
+})
+.then(data => {
+  console.log(data);
+})
+ 
