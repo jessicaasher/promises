76d988f12da20b4d924c94c7cf1d5a5745fdeebb
Jessie A <jasherworks@gmail.com>
2026-07-13 15:25:56 -0700

    remove the console logs

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
 const emailRef = document.querySelector(".email");
-console.log(emailRef)
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
   response.json().then(data => {
-    console.log(data)
     emailRef.innerHTML = data.email
   })
 }) 
